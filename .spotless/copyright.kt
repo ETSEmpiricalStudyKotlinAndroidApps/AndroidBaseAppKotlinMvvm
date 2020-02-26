278b056fe05aa9585ad8ef3b713ec0ea84742f27
tecruz <tiagex.cruz@gmail.com>
2020-02-26 22:07:13 +0000

    Update

--- .spotless/copyright.kt
+++ .spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright $YEAR vmadalin.com
+ * Copyright $YEAR tecruz
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
