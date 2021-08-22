57a78d811abe6d48e8f0d23c5644e5ebdf93c1b1
Tiago Cruz <tiago.cruz@gls-portugal.com>
2021-08-22 16:21:13 +0100

    Increase version

--- buildSrc/src/main/kotlin/BuildAndroidConfig.kt
+++ buildSrc/src/main/kotlin/BuildAndroidConfig.kt
@@ -25,7 +25,7 @@ object BuildAndroidConfig {
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 29
 
-    const val VERSION_CODE = 6
+    const val VERSION_CODE = 7
     const val VERSION_NAME = "1.0"
 
     const val SUPPORT_LIBRARY_VECTOR_DRAWABLES = true
